ccfe7b72011324a768bc3361e235d6c6ea64c19e
Denis Potapov <fantom.r45@mail.ru>
2020-10-19 17:42:05 +0500

    PARALLEL Background Tasks with Kotlin Coroutines (ASYNC AND AWAIT) + classic pattern

--- app/src/main/java/com/example/coroutinesexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesexample/MainActivity.kt
@@ -6,46 +6,39 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
     private val RESULT_1 = "Result #1"
     private val RESULT_2 = "Result #2"
-    private val JOB_TIMEOUT = 2100L
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            setNewText("Click")
-            CoroutineScope(IO).launch {
-                fakeApiRequest()
-            }
-
+            setNewText("Clicked!")
+            fakeApiRequest()
         }
     }
-
-    private suspend fun fakeApiRequest() {
-        withContext(IO) {
-            val job = withTimeoutOrNull(JOB_TIMEOUT) {
-                val result1 = getResultFromApi() // wait
-                setTextOnMainThread("Got $result1")
-
-                val result2 = getResult2FromApi() // wait
-                setTextOnMainThread("Got $result2")
+    private fun fakeApiRequest() {
+        CoroutineScope(IO).launch {
+            val executionTime = measureTimeMillis {
+                val result1: Deferred<String> = async {
+                    println("debug: launching job1: ${Thread.currentThread().name}")
+                    getResultFromApi()
+                }
+                val result2: Deferred<String> = async {
+                    println("debug: launching job2: ${Thread.currentThread().name}")
+                    getResult2FromApi()
+                }
+                setTextOnMainThread("Got ${result1.await()}")
+                setTextOnMainThread("Got ${result2.await()}")
             }
-
-            if (job == null) {
-                val cancelMessage = "Cancelling job...Job took longer than $JOB_TIMEOUT ms"
-                println("debug: $cancelMessage")
-                setTextOnMainThread(cancelMessage)
-
-            }
+            println("debug: total time elapsed: $executionTime")
         }
     }
-
     private fun setNewText(input: String) {
         val newText = text.text.toString() + "\n$input"
         text.text = newText
@@ -57,7 +50,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private suspend fun getResultFromApi(): String {
         logThread("getResultFromApi")
         delay(1000)
@@ -66,7 +58,7 @@ class MainActivity : AppCompatActivity() {
 
     private suspend fun getResult2FromApi(): String {
         logThread("getResult2FromApi")
-        delay(1000)
+        delay(1700)
         return RESULT_2
 
     }
@@ -75,3 +67,33 @@ class MainActivity : AppCompatActivity() {
         println("debug $methodName: ${Thread.currentThread().name}")
     }
 }
+
+// example of how to use the classic job + launch coroutine pattern
+/*
+private fun fakeApiRequest() {
+    val startTime = System.currentTimeMillis()
+    val parentJob = CoroutineScope(IO).launch {
+        val job1 = launch {
+            val time1 = measureTimeMillis {
+                println("debug: launching job1 in thread: ${Thread.currentThread().name}")
+                val result1 = getResultFromApi()
+                setTextOnMainThread("Got $result1")
+
+            }
+            println("debug: completed job1 in $time1 ms")
+        }
+
+        val job2 = launch {
+            val time2 = measureTimeMillis {
+                println("debug: launching job2 in thread: ${Thread.currentThread().name}")
+                val result2 = getResult2FromApi()
+                setTextOnMainThread("Got $result2")
+
+            }
+            println("debug: completed job2 in $time2 ms")
+        }
+    }
+    parentJob.invokeOnCompletion {
+        println("debug: total elapsed time: ${System.currentTimeMillis() - startTime}")
+    }
+}*/
