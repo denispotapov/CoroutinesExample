a9f640e1162d366da473967b7d2860ba0e5499dc
Denis Potapov <fantom.r45@mail.ru>
2020-10-19 11:29:12 +0500

    Network Timeouts

--- app/src/main/java/com/example/coroutinesexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesexample/MainActivity.kt
@@ -3,26 +3,45 @@ package com.example.coroutinesexample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private val RESULT_1 = "Result #1"
     private val RESULT_2 = "Result #2"
+    private val JOB_TIMEOUT = 2100L
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            // IO, Main, Default
+            setNewText("Click")
             CoroutineScope(IO).launch {
                 fakeApiRequest()
+            }
+
+        }
+    }
+
+    private suspend fun fakeApiRequest() {
+        withContext(IO) {
+            val job = withTimeoutOrNull(JOB_TIMEOUT) {
+                val result1 = getResultFromApi() // wait
+                setTextOnMainThread("Got $result1")
+
+                val result2 = getResult2FromApi() // wait
+                setTextOnMainThread("Got $result2")
+            }
+
+            if (job == null) {
+                val cancelMessage = "Cancelling job...Job took longer than $JOB_TIMEOUT ms"
+                println("debug: $cancelMessage")
+                setTextOnMainThread(cancelMessage)
+
             }
         }
     }
@@ -38,14 +57,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private suspend fun fakeApiRequest() {
-        val result1 = getResultFromApi()
-        println("debug $result1")
-        setTextOnMainThread(result1)
-
-        val result2 = getResult2FromApi(result1)
-        setTextOnMainThread(result2)
-    }
 
     private suspend fun getResultFromApi(): String {
         logThread("getResultFromApi")
@@ -53,7 +64,7 @@ class MainActivity : AppCompatActivity() {
         return RESULT_1
     }
 
-    private suspend fun getResult2FromApi(result1: String): String {
+    private suspend fun getResult2FromApi(): String {
         logThread("getResult2FromApi")
         delay(1000)
         return RESULT_2
