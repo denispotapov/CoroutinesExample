8eddc833da9fdae05da7bdacc482d8a76a07776c
Denis Potapov <fantom.r45@mail.ru>
2020-10-20 15:51:30 +0500

    GlobalScope

--- app/src/main/java/com/example/coroutinesexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesexample/MainActivity.kt
@@ -4,44 +4,44 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "TAG"
+    private val TAG = "Debug"
+    private lateinit var parentJob: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        main()
         button.setOnClickListener {
-            main()
+            parentJob.cancel()
         }
     }
 
+    suspend fun work(i: Int) {
+        delay(3000)
+        println("Work $i done. ${Thread.currentThread().name}")
+    }
+
     private fun main() {
-        CoroutineScope(Main).launch {
-            println("Starting job in Thread: ${Thread.currentThread().name}")
-            val result1 = getResult()
-            println("result1: $result1")
-            val result2 = getResult()
-            println("result1: $result2")
-            val result3 = getResult()
-            println("result1: $result3")
-            val result4 = getResult()
-            println("result1: $result4")
-            val result5 = getResult()
-            println("result1: $result5")
-        }
-        CoroutineScope(Main).launch {
-            delay(1000)
-            runBlocking {
-                println("Blocking Thread: ${Thread.currentThread().name}")
-                delay(4000)
-                println("Done blocking Thread: ${Thread.currentThread().name}")
+        val startTime = System.currentTimeMillis()
+        println("Starting parent job...")
+        parentJob = CoroutineScope(Main).launch {
+            GlobalScope.launch {
+                work(1)
+            }
+            GlobalScope.launch {
+                work(2)
+            }
+            parentJob.invokeOnCompletion { throwable ->
+                if (throwable != null) {
+                    println("Job was cancelled after ${System.currentTimeMillis() - startTime} ms")
+                } else {
+                    println("Done in ${System.currentTimeMillis() - startTime} ms")
+                }
             }
         }
     }
